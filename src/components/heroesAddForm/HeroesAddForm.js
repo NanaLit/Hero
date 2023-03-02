c211d5ab49c4553d93b771528d28a52c79af841c
Nana_Lit <nanalit16@gmail.com>
2023-03-02 18:00:54 +0300

    homeWork

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,18 +1,55 @@
 
-
-// Задача для этого компонента:
-// Реализовать создание нового героя с введенными данными. Он должен попадать
-// в общее состояние и отображаться в списке + фильтроваться
-// Уникальный идентификатор персонажа можно сгенерировать через uiid
-// Усложненная задача:
-// Персонаж создается и в файле json при помощи метода POST
-// Дополнительно:
-// Элементы <option></option> желательно сформировать на базе
-// данных из фильтров
+import { useHttp } from "../../hooks/http.hook";
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch, useSelector } from 'react-redux';
+import { heroCreated } from '../../actions';
 
 const HeroesAddForm = () => {
+
+    const [heroName, setHeroName] = useState('');
+    const [heroDescription, setHeroDescription] = useState('');
+    const [heroElement, setHeroElement] = useState('');
+
+    const dispatch = useDispatch();
+    const {filters, filtersLoadingStatus} = useSelector(state => state);
+    const {request} = useHttp();
+
+    const createHero = (e)=> {
+        e.preventDefault();
+        const newHero = {
+            id: uuidv4(),
+            name: heroName,
+            description: heroDescription,
+            element: heroElement
+        }
+        request(`http://localhost:3001/heroes/`, "POST", JSON.stringify(newHero))
+            .then(data => console.log(data, 'Created'))
+            .then(dispatch(heroCreated(newHero)))
+            .catch(err => console.log(err));
+
+        setHeroName('');
+        setHeroDescription('');
+        setHeroElement('');
+    };
+
+    const renderFilters = (filters, status) => {
+        if (status === 'loading') {
+            return <option>Загрузка фильтров</option>
+        } else if (status === 'error') {
+            return <option>Ошибка загрузки</option>
+        }
+
+        if (filters && filters.length > 0) { 
+            
+            return filters.map(({label, name}) => {
+                if (name === 'all') return;
+                return <option key={name} value={name}>{label}</option>
+            })
+        }
+    }
     return (
-        <form className="border p-4 shadow-lg rounded">
+        <form className="border p-4 shadow-lg rounded" onSubmit={createHero}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
                 <input 
@@ -21,6 +58,8 @@ const HeroesAddForm = () => {
                     name="name" 
                     className="form-control" 
                     id="name" 
+                    value={heroName}
+                    onChange={(e) => setHeroName(e.target.value)}
                     placeholder="Как меня зовут?"/>
             </div>
 
@@ -32,7 +71,9 @@ const HeroesAddForm = () => {
                     className="form-control" 
                     id="text" 
                     placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
+                    style={{"height": '130px'}}
+                    value={heroDescription}
+                    onChange={(e) => setHeroDescription(e.target.value)}/>
             </div>
 
             <div className="mb-3">
@@ -41,16 +82,17 @@ const HeroesAddForm = () => {
                     required
                     className="form-select" 
                     id="element" 
-                    name="element">
+                    name="element"
+                    value={heroElement}
+                    onChange={(e) => setHeroElement(e.target.value)}>
                     <option >Я владею элементом...</option>
-                    <option value="fire">Огонь</option>
-                    <option value="water">Вода</option>
-                    <option value="wind">Ветер</option>
-                    <option value="earth">Земля</option>
+                    {
+                        renderFilters(filters, filtersLoadingStatus)
+                    }
                 </select>
             </div>
 
-            <button type="submit" className="btn btn-primary">Создать</button>
+            <button type="submit" className="btn btn-primary" >Создать</button>
         </form>
     )
 }
